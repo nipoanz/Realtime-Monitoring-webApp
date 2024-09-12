b4ed8778951277cfcafba29273eaec7d7ef5e6f1
Nicolas Potier Anzola <nicolaspotier97@gmail.com>
2024-09-11 19:33:49 -0500

    feature: update ip address

--- IOTMonitoringServer/IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/IOTMonitoringServer/settings.py
@@ -167,8 +167,8 @@ MQTT_USER = "admin"
 MQTT_PASSWORD = "admin"
 
 # Credenciales para el usuario publicador del bróker MQTT
-MQTT_USER_PUB = "admin2"
-MQTT_PASSWORD_PUB = "admin2"
+MQTT_USER_PUB = "adminN"
+MQTT_PASSWORD_PUB = "Asasesbgghdfe423."
 
 # Tópico a suscribir. "+/+/+/+/out" se suscribe únicamente a los
 # tópicos con forma <país>/<estado>/<ciudad>/<usuario>/out.

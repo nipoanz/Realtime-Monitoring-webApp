8b30fd4addfdcc268a4e453bafd4fab8f48a2a58
Nicolas Potier Anzola <nicolaspotier97@gmail.com>
2024-09-16 02:15:21 -0500

    update alert

--- IOTMonitoringServer/control/monitor.py
+++ IOTMonitoringServer/control/monitor.py
@@ -54,12 +54,83 @@ def analyze_data():
             message = "ALERT {} {} {}".format(variable, min_value, max_value)
             topic = '{}/{}/{}/{}/in'.format(country, state, city, user)
             print(datetime.now(), "Sending alert to {} {}".format(topic, variable))
+            print("Mensaje: ", message)
+            setup_mqtt()
             client.publish(topic, message)
             alerts += 1
 
     print(len(aggregation), "dispositivos revisados")
     print(alerts, "alertas enviadas")
+    
 
+def analyze_temp_average():
+    '''
+    Analiza el promedio de la temperatura y envía alertas si está fuera de los límites.
+    '''
+    print("Calculando alertas de temperatura...")
+
+    # Consulta solo para la variable 'temperatura' y últimos 2 minutos
+    data = Data.objects.filter(
+        base_time__gte=datetime.now() - timedelta(minutes=2),
+        measurement__name="temperatura"  # Filtra solo la temperatura
+    )
+
+    # Agrupa y promedia los datos
+    aggregation = data.annotate(check_value=Avg('avg_value')) \
+        .select_related('station', 'measurement') \
+        .select_related('station__user', 'station__location') \
+        .select_related('station__location__city', 'station__location__state',
+                        'station__location__country') \
+        .values('check_value', 'station__user__username',
+                'measurement__name',
+                'measurement__max_value',
+                'measurement__min_value',
+                'station__location__city__name',
+                'station__location__state__name',
+                'station__location__country__name')
+
+    alerts = 0
+    for item in aggregation:
+        led_state = "off"  # Estado inicial del LED
+
+        variable = item["measurement__name"]
+        max_value = item["measurement__max_value"] or 0
+        min_value = item["measurement__min_value"] or 0
+        print("Max value: ", max_value)
+        print("Min value: ", min_value)
+        
+
+        country = item['station__location__country__name']
+        state = item['station__location__state__name']
+        city = item['station__location__city__name']
+        user = item['station__user__username']
+
+        check_value = item["check_value"]
+        print("Average value: ", item["check_value"])
+
+        # Evaluar la medición en relación con los límites
+        if check_value > max_value:
+            led_state = "slow_blink" 
+        elif check_value < min_value:
+            led_state = "fast_blink"
+        else:
+            led_state = "off"
+            
+        print("Alert state: ", led_state)
+
+        message = "state: {}".format(led_state)
+        topic = '{}/{}/{}/{}/in'.format(country, state, city, user)
+        print(datetime.now(), "Sending alert to {} {}".format(topic, variable))
+        print("Mensaje: ", message)
+        setup_mqtt()
+        client.publish(topic, message)
+
+        alerts += 1
+
+    print(len(aggregation), "dispositivos revisados")
+    print(alerts, "alertas enviadas")
+
+    
 
 def on_connect(client, userdata, flags, rc):
     '''
@@ -82,7 +153,6 @@ def setup_mqtt():
     '''
     Configura el cliente MQTT para conectarse al broker.
     '''
-
     print("Iniciando cliente MQTT...", settings.MQTT_HOST, settings.MQTT_PORT)
     global client
     try:
@@ -104,10 +174,13 @@ def setup_mqtt():
 
 def start_cron():
     '''
-    Inicia el cron que se encarga de ejecutar la función analyze_data cada minuto.
+    Inicia el cron que se encarga de ejecutar la función analyze_data cada hora.
+    Initia el cron que se encarga de ejecutar la función analyze_temp_average cada minuto.
     '''
-    print("Iniciando cron...")
+    print("Iniciando cron de alertas generales...")
     schedule.every().hour.do(analyze_data)
+    print("Iniciando Cron de alerta promedio de temperatura...")
+    schedule.every(1).minutes.do(analyze_temp_average)
     print("Servicio de control iniciado")
     while 1:
         schedule.run_pending()

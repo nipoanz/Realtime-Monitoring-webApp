61d6e92961c7ecf5a66c8e46dcc30f04cff9311f
Nicolas Potier Anzola <nicolaspotier97@gmail.com>
2024-09-15 23:45:40 -0500

    update ip address

--- IOTMonitoringServer/IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/IOTMonitoringServer/settings.py
@@ -163,7 +163,7 @@ MQTT_HOST = "34.224.27.182"
 MQTT_PORT = 8082
 
 # Credenciales para el usuario suscriptor del bróker MQTT
-MQTT_USER = "peperez"
+MQTT_USER = "admin"
 MQTT_PASSWORD = "Uniandinos2024"
 
 # Credenciales para el usuario publicador del bróker MQTT

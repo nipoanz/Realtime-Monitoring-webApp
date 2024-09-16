696af40398152d38b7a76261a2f4032b98727390
Nicolas Potier Anzola <nicolaspotier97@gmail.com>
2024-09-16 03:21:00 -0500

    update ip address

--- IOTMonitoringServer/control/monitor.py
+++ IOTMonitoringServer/control/monitor.py
@@ -189,5 +189,6 @@ def start_cron():
     schedule.every(1).minutes.do(analyze_temp_average)
     print("Servicio de control iniciado")
     while 1:            
-        schedule.run_pending()
-        time.sleep(1)
+        # schedule.run_pending()
+        analyze_temp_average()
+        time.sleep(5)
